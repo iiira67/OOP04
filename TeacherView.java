39053019f4cbc6e177f71463ea1a001172326b3d
iiira67 <137424567+iiira67@users.noreply.github.com>
2024-05-06 11:05:52 +0300

    Create TeacherView.java

---
+++ TeacherView.java
@@ -0,0 +1,14 @@
+package OOP04;
+
+import java.util.List;
+
+public class TeacherView implements UserView <Teacher> {
+
+    @Override
+    public void sendOnConsole(List <Teacher> list) {
+        for (Teacher teacher : list) {
+            System.out.println(teacher);
+            
+        }        
+    }    
+}
